9cb9bac42fb848f639d91cb6c065db1a9e859766
KayoRonald <kayoronald4321@gmail.com>
2023-11-30 14:14:17 -0300

    corrigido bug letras

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ document.getElementById("calcular").addEventListener("click", () =>{
   const valor1 = Number(document.getElementById("valor-1").value);
   const valor2 = Number(document.getElementById("valor-2").value);
   const operador = document.getElementById("operador").value;
+
+  if (isNaN(valor1) || isNaN(valor2)) {
+    Swal.fire("Por favor, insira valores numéricos");
+    return;
+  }
+
   let resultado = 0;
   switch (operador) {
     case "+":
